a0a20ce50a56afd26ad94c454a42b41903ba9d84
Grigory Trubnikov <g.trubnikov@mail.ru>
2019-11-25 18:55:30 +0300

    Update

--- src/com/lesson14/AlphabetComparator.java
+++ src/com/lesson14/AlphabetComparator.java
@@ -6,20 +6,24 @@ public class AlphabetComparator implements Comparator<String> {
 
   @Override
   public int compare(String a, String b) {
-    var aLength = a.length();
-    var bLength = b.length();
-    
-    return aLength < bLength ? -1:0;
-    
-//    if (aLength < bLength) {
-//      return -1;
-//    }
-//  if (aLength > bLength) {
-//  return 1;
-//  }
-//   
-//    return 0;
-//    return a.compareTo(b);
+
+    return Integer.parseInt(a) - Integer.parseInt(b);
+
+
+    // var aLength = a.length();
+    // var bLength = b.length();
+    //
+    // return aLength < bLength ? -1:0;
+
+    // if (aLength < bLength) {
+    // return -1;
+    // }
+    // if (aLength > bLength) {
+    // return 1;
+    // }
+    //
+    // return 0;
+    // return a.compareTo(b);
   }
 
 }
