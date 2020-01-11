46c529d57628839c3fd46e196574919722eaca21
Grigory Trubnikov <g.trubnikov@mail.ru>
2020-01-11 15:59:54 +0300

    update

--- Homework12/src/main/java/com/homework12/exceptions/MissmatchCharException.java
+++ Homework12/src/main/java/com/homework12/exceptions/MissmatchCharException.java
@@ -1,7 +1,7 @@
 package com.homework12.exceptions;
 
 public class MissmatchCharException extends Exception {
-	public MissmatchCharException() {
-		super("Вы ввели неправильное слово и проиграли");
-	}
+  public MissmatchCharException() {
+    super("Вы ввели неправильное слово и проиграли");
+  }
 }
--- Homework12/src/main/java/com/homework12/exceptions/NoInputException.java
+++ Homework12/src/main/java/com/homework12/exceptions/NoInputException.java
@@ -1,7 +1,7 @@
 package com.homework12.exceptions;
 
 public class NoInputException extends Exception {
-	public NoInputException() {
-		super("Вы ничего не ввели и проиграли");
-}
+  public NoInputException() {
+    super("Вы ничего не ввели и проиграли");
+  }
 }
