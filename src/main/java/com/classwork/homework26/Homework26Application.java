c53499beb12c6f7f1af1bf90bce2fc9745e933e4
Grigory Trubnikov <g.trubnikov@mail.ru>
2020-02-16 18:33:59 +0300

    Homework26

--- src/main/java/com/classwork/homework26/Homework26Application.java
+++ src/main/java/com/classwork/homework26/Homework26Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Homework26Application implements CommandLineRunner {
 
+
   public static void main(String[] args) {
     SpringApplication.run(Homework26Application.class, args);
   }
